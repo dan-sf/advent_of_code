b156ca657c4e901dd8ae4a5a7d4e0474296bb2f7
dan-sf <fowler.dn@gmail.com>
2019-08-20 20:58:08 -0700

    Solve rust day6 solution1

--- day6/solution1.rs
+++ day6/solution1.rs
@@ -1,15 +1,27 @@
 use std::fs;
 use std::io;
 use std::io::BufRead;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
 #[derive(Debug)]
+#[derive(Hash)]
+#[derive(Copy, Clone)]
 struct Point {
     x: i32,
     y: i32,
 }
 
-fn construct_grid(coords: &Vec<Point>) -> Vec<Vec<i32>> {
-    let mut grid: Vec<Vec<i32>> = Vec::new();
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl Eq for Point { }
+
+fn construct_grid(coords: &Vec<Point>) -> Vec<Vec<Option<Point>>> {
+    let mut grid: Vec<Vec<Option<Point>>> = Vec::new();
     let mut max_x = 0;
     let mut max_y = 0;
 
@@ -25,11 +37,44 @@ fn construct_grid(coords: &Vec<Point>) -> Vec<Vec<i32>> {
     // We need to add 1 to the max x and y to account for the origin
     max_x += 1;
     max_y += 1;
-    for _i in 0..max_y {
-        grid.push(vec![0;max_x as usize]);
+    for _ in 0..max_y {
+        grid.push(vec![None;max_x as usize]);
     }
 
     grid
+}
+
+fn get_manhattan_dist(start: &Point, end: &Point) -> i32 {
+    let output = i32::abs(start.x - end.x) + i32::abs(start.y - end.y);
+    output
+}
+
+fn fill_grid(grid: &mut Vec<Vec<Option<Point>>>, coords: &Vec<Point>) {
+    for row in 0..grid.len() {
+        for col in 0..grid[0].len() {
+            let cur_point = Point { x: col as i32, y: row as i32 };
+            let mut min_dist: i32 = (grid.len() + grid[0].len()) as i32;
+            let mut min_point = Point { x: 0, y: 0 };
+            let mut mins = Vec::new();
+            for p in coords.iter() {
+                let dist = get_manhattan_dist(&cur_point, &p);
+                if dist <= min_dist {
+                    min_dist = dist;
+                    min_point.x = p.x;
+                    min_point.y = p.y;
+                    mins.push(min_dist)
+                }
+            }
+            let mut cmins = HashMap::new();
+            for i in mins.iter() {
+                let c = cmins.entry(i).or_insert(0);
+                *c += 1;
+            }
+            if cmins[&min_dist] <= 1 {
+                grid[row][col] = Some(min_point);
+            }
+        }
+    }
 }
 
 fn load_coordinates() -> Vec<Point> {
@@ -50,10 +95,45 @@ fn load_coordinates() -> Vec<Point> {
     output
 }
 
-fn main() {
-    let coords = load_coordinates();
-    let grid = construct_grid(&coords);
-    println!("coords: {:?}, grid: {:?}", coords, grid[0]);
+fn get_largest_area(grid: Vec<Vec<Option<Point>>>) -> i32 {
+    let mut infinite_set: HashSet<Point> = HashSet::new();
+    for i in 0..grid[0].len() {
+        if let Some(p) = &grid[0][i] {
+            infinite_set.insert(*p);
+        }
+        if let Some(p) = &grid[grid.len()-1][i] {
+            infinite_set.insert(*p);
+        }
+    }
+    for j in 0..grid.len() {
+        if let Some(p) = &grid[j][0] {
+            infinite_set.insert(*p);
+        }
+        if let Some(p) = &grid[j][grid[0].len()-1] {
+            infinite_set.insert(*p);
+        }
+    }
+
+    let mut count = HashMap::new();
+    for row in 0..grid.len() {
+        for col in 0..grid[0].len() {
+            if let Some(p) = grid[row][col] {
+                if !infinite_set.contains(&p) {
+                    let c = count.entry(p).or_insert(0);
+                    *c += 1;
+                }
+            }
+        }
+    }
+
+    *count.values().max().unwrap()
 }
 
+fn main() {
+    let coords = load_coordinates();
+    let mut grid = construct_grid(&coords);
+    fill_grid(&mut grid, &coords);
+    let largest_area = get_largest_area(grid);
+    println!("Size of the largest area: {}", largest_area);
+}
 
