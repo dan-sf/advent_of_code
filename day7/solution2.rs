0d653c0ef4ac4fd06ea9aeb5afbde2e7f35c3d66
dan-sf <fowler.dn@gmail.com>
2019-08-25 11:09:35 -0700

    Solve day7 problem 2 rust

--- day7/solution2.rs
+++ day7/solution2.rs
@@ -4,7 +4,116 @@ use std::io::BufRead;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
+// Create worker and work structs to keep track of the working state as we iterate over the tasks
+
+#[derive(Clone)]
+struct Worker {
+    idle: bool,
+    step: Option<char>,
+    seconds_left: i32,
+}
+
+impl Worker {
+    fn new() -> Worker {
+        Worker {
+            idle: true,
+            step: None,
+            seconds_left: 0,
+        }
+    }
+}
+
+struct Work {
+    workers: Vec<Worker>,
+    total_time: i32,
+}
+
+impl Work {
+    fn new() -> Work {
+        Work {
+            workers: vec![Worker::new();5],
+            total_time: 0,
+        }
+    }
+
+    fn doing_work(&self) -> bool {
+        // Return true if any worker is busy
+        for w in self.workers.iter() {
+            if !w.idle {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn workers_available(&self) -> bool {
+        // Return true if any worker is available
+        for w in self.workers.iter() {
+            if w.idle {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn increment_workers(&mut self, graph: &mut HashMap<char, HashSet<char>>, total_nodes: &mut HashSet<char>) {
+        // Move the workers forward that have work by counting down the time spent on the worker's
+        // current step if it has one. Also, finish the worker's task once it has spent all time
+        // needed on it
+        for w in self.workers.iter_mut() {
+            // If the second_left is 1, we know that this iteration is the last one needed to
+            // finish the task. So we can free up the worker, if we went to 0 we would be double
+            // counting
+            if w.seconds_left > 1 {
+                w.seconds_left -= 1;
+            } else {
+                w.idle = true;
+                // If there is a task on this worker we should finish it by removing the node from
+                // the set of nodes that need to be worked on
+                if let Some(n) = w.step {
+                    remove_node(graph, total_nodes, n);
+                }
+                w.step = None;
+            }
+        }
+    }
+
+    fn give_task(&mut self, task: char) {
+        // Take a task (a char step) and give it to an available worker. We also set the amount of
+        // time that is needed to finish the step here
+        for w in self.workers.iter_mut() {
+            if w.idle {
+                w.idle = false;
+                w.step = Some(task);
+                // Ascii 'A' is 65, so we minus 64 so for 'A' we would get 1 + 60, 'B' 2 + 60, and
+                // so on
+                w.seconds_left = task as i32 - 64 + 60;
+                break;
+            }
+        }
+    }
+
+    fn working_tasks(&self) -> HashSet<char> {
+        // Loop through the workers and check to see what is currently being worked on
+        let mut output = HashSet::new();
+        for w in self.workers.iter() {
+            if let Some(n) = w.step {
+                output.insert(n.clone());
+            }
+        }
+        output
+    }
+
+    // @Debug: Function used to help track down an off by one error in increment_workers function.
+    // This debug print was critical in being able to solve the problem, so I'm leaving it in
+    fn _print_worker_sec(&self) {
+        println!("{:?}", self.workers.iter().map(|w| (w.seconds_left, w.step)).collect::<Vec<(i32, Option<char>)>>());
+    }
+}
+
 fn create_graph() -> (HashMap<char, HashSet<char>>, HashSet<char>) {
+    // Here we create a HashMap of the step to a set of that steps dependencies. We also output a
+    // set of all steps
     let mut output_map: HashMap<char, HashSet<char>> = HashMap::new();
     let mut output_set: HashSet<char> = HashSet::new();
 
@@ -27,8 +136,8 @@ fn create_graph() -> (HashMap<char, HashSet<char>>, HashSet<char>) {
     (output_map, output_set)
 }
 
-// Here we should get as many nodes as we can in relation to the workers
-fn work_on_ready_tasks(graph: &HashMap<char, HashSet<char>>, total_nodes: &HashSet<char>, work: &mut Work) -> char {
+fn give_workers_work(graph: &mut HashMap<char, HashSet<char>>, total_nodes: &mut HashSet<char>, work: &mut Work) {
+    // Loop through all nodes checking to see which ones are ready to be picked up by workers
     let mut can_run: Vec<char> = Vec::new();
     for n in total_nodes.iter() {
         if !graph.contains_key(n) {
@@ -38,64 +147,45 @@ fn work_on_ready_tasks(graph: &HashMap<char, HashSet<char>>, total_nodes: &HashS
         }
     }
 
+    // If no steps are available to run we exit early
     if can_run.len() == 0 {
-        return false;
+        return;
     }
 
-    // While we have items in can run, we should iterate on the Work/Workers and increment time until can run is done.
+    // While there are tasks to run and workers available, send the tasks to the open workers
     can_run.sort();
-    while can_run.len() > 0 {
-        if work.has_available_worker() {
-            work.give_task(can_run.pop(0));
-        }
-        else {
-            work.work_until_worker_available();
+    while can_run.len() > 0 && work.workers_available() {
+        let task = can_run.remove(0);
+        // We should only add the task to the worker if its not already being worked on. The nodes
+        // do not get removed from the graph (or total_nodes) until a worker has finished its work
+        // for the step
+        if !work.working_tasks().contains(&task) {
+            work.give_task(task.clone());
         }
     }
-    true
 }
 
-
-fn visit_node(graph: &mut HashMap<char, HashSet<char>>, node: char) {
+fn remove_node(graph: &mut HashMap<char, HashSet<char>>, total_nodes: &mut HashSet<char>, node: char) {
+    // Remove the node/step from the graph and total_nodes
     graph.remove(&node);
     for (_, deps) in graph.iter_mut() {
         deps.remove(&node);
     }
+
+    total_nodes.remove(&node);
 }
-
-
-struct Worker {
-    idle: bool,
-    step: Option<char>,
-    seconds_left: i32,
-}
-
-struct Work {
-    workers: Vec<Worker>,
-    total_time: i32,
-}
-
-// get_working_set -> ['a','b','c', ...] HashSet
-// new -> init the workers
-// 
 
 fn main() {
-    let mut output: Vec<char> = Vec::new();
+    let mut work = Work::new();
     let (mut graph, mut total_nodes) = create_graph();
-    while !graph.is_empty() {
-
-        work_on_tasks
-
-
-
-
-        let node = get_next_node(&graph, &total_nodes);
-        output.push(node);
-        total_nodes.remove(&node);
-        visit_node(&mut graph, node);
+    while !graph.is_empty() || work.doing_work() {
+        if work.workers_available() {
+            give_workers_work(&mut graph, &mut total_nodes, &mut work);
+        }
+        // work._print_worker_sec();
+        work.increment_workers(&mut graph, &mut total_nodes);
+        work.total_time += 1;
     }
-    let mut order = String::new();
-    let _ = output.iter().map(|n| order.push(*n)).collect::<Vec<_>>();
-    println!("Instruction order: {}", order);
+    println!("Total time: {}", work.total_time);
 }
 
