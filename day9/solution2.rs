28f593f8f86d58dc1493c9af7e24e12433c4cea4
dan-sf <fowler.dn@gmail.com>
2019-08-31 18:09:53 -0700

    Solve problem2 day9

--- day9/solution2.rs
+++ day9/solution2.rs
@@ -1,31 +1,26 @@
 use std::fs;
 use std::io::Read;
+use std::collections::VecDeque;
 
-fn play_game(players: i32, last_marble: i32) -> i32 {
-    let mut player_scores: Vec<i32> = vec![0;players as usize];
+// To solve this day I needed to update to using a VecDeque which simplifies the code quite a bit
+// (didn't think of it for problem1), it also more closely models the problem. And I had to use i64
+// ints over i32
+
+fn play_game(players: i64, last_marble: i64) -> i64 {
+    let mut player_scores: Vec<i64> = vec![0;players as usize];
     let mut player_index: usize = 3;
 
-    let mut circle: Vec<i32> = vec![0, 2, 1];
-    let mut current: i32 = 1;
+    let mut circle: VecDeque<i64> = VecDeque::new();
+    circle.push_back(0); circle.push_back(2); circle.push_back(1);
 
     for marble in 3..(last_marble+1) {
         if marble % 23 == 0 {
-            let mut remove_index = current - 7;
-            if remove_index < 0 {
-                remove_index += circle.len() as i32;
-            }
-            player_scores[player_index] += marble + circle.remove(remove_index as usize);
-            current = remove_index;
+            circle.rotate_right(7);
+            player_scores[player_index] += marble + circle.pop_back().unwrap();
+            circle.rotate_left(1);
         } else {
-            current += 2;
-            if current as usize > circle.len() {
-                if current as usize == circle.len() {
-                    current = 0;
-                } else {
-                    current = 1;
-                }
-            }
-            circle.insert(current as usize, marble);
+            circle.rotate_left(1);
+            circle.push_back(marble);
         }
 
         player_index += 1;
@@ -41,11 +36,8 @@ fn main() {
     let mut read_string = String::new();
     input.read_to_string(&mut read_string).unwrap();
     let split_input = read_string.as_str().split(' ').collect::<Vec<&str>>();
-    let players = split_input[0].parse::<i32>().unwrap();
-    let last_marble = split_input[6].parse::<i32>().unwrap();
-
-    for i in 10..300 {
-        println!("Winning score: {} {}", play_game(players, i), i);
-    }
+    let players = split_input[0].parse::<i64>().unwrap();
+    let last_marble = split_input[6].parse::<i64>().unwrap();
+    println!("Winning score: {} ", play_game(players, last_marble*100));
 }
 
