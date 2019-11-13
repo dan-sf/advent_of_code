ea2d7d6100dc76f22422e3b8b5d47d0bfc82bfd2
dan-sf <fowler.dn@gmail.com>
2019-11-12 20:26:33 -0800

    Start getting attack code together

--- day24/solution1.rs
+++ day24/solution1.rs
@@ -1,6 +1,7 @@
 use std::io::{BufReader, BufRead};
 use std::fs::File;
 use std::collections::HashSet;
+//use std::cell::Ref
 
 
 #[derive(Debug, PartialEq, Eq)]
@@ -12,6 +13,12 @@ enum AttackType {
     Slashing,
 }
 
+#[derive(Debug, Copy, Clone)]
+enum GroupType {
+    ImmuneSystem,
+    Infection,
+}
+
 #[derive(Debug)]
 struct Group {
     units: i32,
@@ -21,6 +28,7 @@ struct Group {
     attack_type: AttackType,
     damage: i32,
     initiative: i32,
+    group_type: GroupType,
 }
 
 fn get_attack_type_from_text(text: &String) -> AttackType {
@@ -63,6 +71,7 @@ fn parse_input(path: &str) -> (Vec<Group>, Vec<Group>) {
 
     let mut push_immune = true;
     let mut line_iter = reader.lines();
+    let mut id: usize = 0;
     while let Some(line) = line_iter.next() {
         let split = line.unwrap().split(" ").map(|r| r.to_string()).collect::<Vec<String>>();
         if &split[0] == "" {
@@ -129,7 +138,8 @@ fn parse_input(path: &str) -> (Vec<Group>, Vec<Group>) {
             immunities: immunities,
             attack_type: attack_type,
             damage: damage,
-            initiative: initiative
+            initiative: initiative,
+            group_type: if push_immune { GroupType::ImmuneSystem } else { GroupType::Infection },
         };
 
         if push_immune {
@@ -137,77 +147,129 @@ fn parse_input(path: &str) -> (Vec<Group>, Vec<Group>) {
         } else {
             infection.push(group);
         }
+
+        id += 1;
     }
     (immune_system, infection)
 }
 
-fn get_selection(select_group: &Group, target_groups: &Vec<Group>, filter: &HashSet<usize>) -> Option<usize> {
+fn get_selection(select_group: &Group, target_groups: &Vec<Group>, filter: &HashSet<usize>) -> Option<(usize, i32)> {
     let mut targets: Vec<Group> = vec![];
-    let mut output: Option<usize> = None;
+    let mut output: Option<(usize, i32)> = None;
     let mut max_damage = 0;
     let mut max_initiative = 0;
+    let mut max_ep = 0;
 
-    for (i, target) in target_groups.iter().enumerate() {
-        if target.immunities.contains(&select_group.attack_type) || filter.contains(&i) {
-            continue;
-        }
+    if select_group.units > 0 {
+        for (i, target) in target_groups.iter().enumerate() {
+            if target.immunities.contains(&select_group.attack_type) || filter.contains(&i) || target.units <= 0 {
+                continue;
+            }
 
-        let mut multiplier = 1;
-        if target.weaknesses.contains(&select_group.attack_type) {
-            multiplier = 2;
-        }
+            let mut multiplier = 1;
+            if target.weaknesses.contains(&select_group.attack_type) {
+                multiplier = 2;
+            }
 
-        let attack_damage = multiplier * select_group.damage * select_group.units;
-        if attack_damage > max_damage {
-            max_damage = attack_damage;
-            max_initiative = target.initiative;
-            output = Some(i);
-        } else if attack_damage == max_damage && max_initiative < target.initiative {
-            max_initiative = target.initiative;
-            output = Some(i);
+            let target_ep = target.units * target.damage;
+
+            let attack_damage = multiplier * select_group.damage * select_group.units;
+            if attack_damage > max_damage {
+                max_damage = attack_damage;
+                max_initiative = target.initiative;
+                max_ep = target_ep;
+                output = Some((i, max_damage));
+            } else if attack_damage == max_damage { // @Note: make sure to test this...
+                if target_ep > max_ep {
+                    max_initiative = target.initiative;
+                    max_ep = target_ep;
+                    output = Some((i, max_damage));
+                } else if target_ep == max_ep && target.initiative > max_initiative {
+                    max_initiative = target.initiative;
+                    output = Some((i, max_damage));
+                }
+            }
         }
     }
     output
 }
 
-fn sort_groups(mut groups: Vec<Group>) -> Vec<Group> {
+//fn sort_groups(mut groups: Vec<Group>) -> Vec<Group> {
+fn sort_groups(groups: &mut Vec<Group>) {
     for i in 0..groups.len() {
-        let base_ef = groups[i].units * groups[i].damage;
+        let base_ep = groups[i].units * groups[i].damage;
         for j in 0..(groups.len()-i) {
-            let check_ef = groups[j].units * groups[j].damage;
-            if (base_ef > check_ef) || (base_ef == check_ef && groups[i].initiative > groups[j].initiative) {
+            let check_ep = groups[j].units * groups[j].damage;
+            if (base_ep > check_ep) || (base_ep == check_ep && groups[i].initiative > groups[j].initiative) {
                 let larger = groups.remove(i);
                 groups.insert(j, larger);
             }
         }
     }
-    groups
 }
 
-fn run_selections(select_groups: &Vec<Group>, target_groups: &Vec<Group>) -> Vec<(usize, usize)> {
-    let mut output: Vec<(usize, usize)> = vec![];
+fn run_selections(select_groups: &Vec<Group>, target_groups: &Vec<Group>) -> Vec<(usize, usize, i32)> {
+    let mut output: Vec<(usize, usize, i32)> = vec![];
     let mut filter: HashSet<usize> = HashSet::new();
 
     for (i, sg) in select_groups.iter().enumerate() {
-        if let Some(target_index) = get_selection(sg, target_groups, &filter) {
+        if let Some(target_index_damage) = get_selection(sg, target_groups, &filter) {
+            let (target_index, target_damage) = target_index_damage;
             filter.insert(target_index);
-            output.push((i, target_index));
+            output.push((i, target_index, target_damage));
         }
     }
     output
 }
 
+//fn set_selections(select_groups: &Vec<Group>, target_groups: &Vec<Group>) -> Vec<(usize, usize)> {
+//    let mut output: Vec<(usize, usize)> = vec![];
+//    let mut filter: HashSet<usize> = HashSet::new();
+//
+//    for (i, sg) in select_groups.iter().enumerate() {
+//        if let Some(target_index) = get_selection(sg, target_groups, &filter) {
+//            filter.insert(target_index);
+//            output.push((i, target_index));
+//        }
+//    }
+//    output
+//}
+
 fn main() {
-    let (immune_system, infection) = parse_input("input.test.txt");
-    println!("{:?}", immune_system);
-    println!();
-    println!("{:?}", infection);
-    let mut filter: HashSet<usize> = HashSet::new();
-    //filter.insert(1);
-    let infection = sort_groups(infection);
-    let immune_system = sort_groups(immune_system);
-    println!("{:?}", get_selection(&immune_system[0], &infection, &filter));
-    // @Note: I will need to sort the groups by effective power and initiative descending (before every selection phase)
-    println!("{:?}", run_selections(&immune_system, &infection));
+    let (mut immune_system, mut infection) = parse_input("input.test.txt");
+    let mut count = 0;
+    loop {
+        let mut filter: HashSet<usize> = HashSet::new();
+        sort_groups(&mut infection);
+        sort_groups(&mut immune_system);
+
+        let mut attack_order: Vec<(i32, usize, usize, i32, GroupType)> = vec![];
+        let sel_indices = run_selections(&immune_system, &infection);
+        for (im_index, in_index, damage) in sel_indices.iter() {
+            attack_order.push((immune_system[*im_index].initiative, *im_index, *in_index, *damage, immune_system[*im_index].group_type));
+        }
+        let sel_indices = run_selections(&infection, &immune_system);
+        for (in_index, im_index, damage) in sel_indices.iter() {
+            attack_order.push((infection[*in_index].initiative, *in_index, *im_index, *damage, infection[*in_index].group_type));
+        }
+        attack_order.sort_by_key(|k| -k.0); // Reverse sort by initiative
+        println!("attack order: {:?}", attack_order);
+
+        for attack in attack_order.iter() {
+            // here we attack ...
+            let (init, attack_ind, target_ind, damage, group_type) = attack;
+            match group_type {
+                GroupType::ImmuneSystem => { infection[*target_ind].units -= (damage / infection[*target_ind].hit_points); println!("infection units: {}", (damage / infection[*target_ind].hit_points)); },
+                GroupType::Infection => { immune_system[*target_ind].units -= (damage / infection[*target_ind].hit_points); },
+            };
+        }
+        println!("immune system: {:?}", immune_system);
+        println!("infection: {:?}", infection);
+        //println!("{:?}", run_selections(&immune_system, &infection));
+        if count > 10 {
+            break;
+        }
+        count += 1;
+    }
 }
 
