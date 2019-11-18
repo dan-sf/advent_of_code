c41a833e323beccbd07d4b1aa829cb2da90cb625
dan-sf <fowler.dn@gmail.com>
2019-11-18 08:46:27 -0800

    Progress on solution2 day23

--- day23/solution2.rs
+++ day23/solution2.rs
@@ -1,5 +1,6 @@
 use std::io::{BufReader, BufRead};
 use std::fs::File;
+use std::collections::HashSet;
 
 #[derive(Debug)]
 struct NanoBot {
@@ -23,9 +24,66 @@ fn parse_input(path: &str) -> Vec<NanoBot> {
 }
 
 fn get_range(a: &NanoBot, b: &NanoBot) -> i32 {
-    i32::abs(a.pos.0 - b.pos.0) +
-    i32::abs(a.pos.1 - b.pos.1) +
-    i32::abs(a.pos.2 - b.pos.2)
+    // i32::abs(a.pos.0 - b.pos.0) +
+    // i32::abs(a.pos.1 - b.pos.1) +
+    // i32::abs(a.pos.2 - b.pos.2)
+    get_range_pos(a.pos, b.pos)
+}
+
+fn get_range_pos(a: (i32, i32, i32), b: (i32, i32, i32)) -> i32 {
+    i32::abs(a.0 - b.0) +
+    i32::abs(a.1 - b.1) +
+    i32::abs(a.2 - b.2)
+}
+
+fn sum_point(pos: (i32, i32, i32)) -> i32 {
+    pos.0 + pos.1 + pos.2
+}
+
+fn get_bots_in_range(pos: (i32, i32, i32), nano_bots: &Vec<NanoBot>) -> i32 {
+    let mut output = 0;
+    for bot in nano_bots.iter() {
+        if get_range_pos(pos, bot.pos) <= bot.radius {
+            output += 1;
+        }
+    }
+    output
+}
+
+fn point_search(start: (i32, i32, i32), mut bot_count: i32, nano_bots: &Vec<NanoBot>) -> (i32, i32, i32) {
+
+    let mut output_point = start;
+    let mut visited: HashSet<(i32, i32, i32)> = HashSet::new();
+    let mut queue: Vec<(i32, (i32, i32, i32))> = Vec::new();
+    queue.push((bot_count, start));
+
+    while !queue.is_empty() {
+        let (cur_bot_count, cur_pos) = queue.pop().unwrap();
+        visited.insert(cur_pos);
+
+        if cur_bot_count < bot_count {
+            continue;
+        }
+
+        for step in &[(1, 0, 0), (0, 1, 0), (0, 0, 1),
+                      (-1, 0, 0), (0, -1, 0), (0, 0, -1)] {
+            let new_pos = (cur_pos.0 + step.0, cur_pos.1 + step.1, cur_pos.2 + step.2);
+            let new_bot_count = get_bots_in_range(new_pos, nano_bots);
+
+            if new_bot_count > bot_count {
+                output_point = new_pos;
+                bot_count = new_bot_count;
+            } else if new_bot_count < bot_count {
+                continue;
+            } else if new_bot_count == bot_count && sum_point(new_pos) < sum_point(output_point) {
+                output_point = new_pos;
+            }
+
+            queue.push((bot_count, new_pos));
+        }
+    }
+
+    output_point
 }
 
 fn main() {
@@ -49,6 +107,9 @@ fn main() {
     }
 
     println!("max_bots: {}, max_bot: {:?}", max_bots, max_bot);
+
+    let result = point_search(max_bot.pos, max_bots, &nano_bots);
+    println!("output_point: {:?}", result);
 
     //let mut count = 1;
     //for bot in nano_bots.iter() {
